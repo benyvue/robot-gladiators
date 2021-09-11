e4c362f7884ef1b875df009d956e8e1349606946
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-09-11 13:31:59 -0500

    added shorthand for store options

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,9 +36,6 @@ var fightOrSkip = function() {
 };
 
 
-
-
-
 var fight = function(enemy) {
   console.log(enemy);
 
@@ -164,25 +161,22 @@ var endGame = function() {
 var shop = function () {
   //Ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
 
+  shopOptionPrompt = parseInt(shopOptionPrompt);
 
   //Use switch to carry out action
   switch (shopOptionPrompt) {
-
-    case "REFILL": //New case
-    case "refill":
+    case 1:
       playerInfo.refillHealth();
       break;
     
-    case "UPGRADE": //New case
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
 
-    case "LEAVE": //New case
-    case "leave":
+    case 3:
       window.alert("Leaving the store.");
       //Do nothing, so function will end
       break;
