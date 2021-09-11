87d98cd97a60403fbe35d9d8ef0875e652d9f52a
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-09-11 15:43:49 -0500

    added high score system

--- assets/js/game.js
+++ assets/js/game.js
@@ -152,7 +152,22 @@ endGame();
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!")
 
-  //If palyer is still alive, player wins!
+  //Check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore"); 
+  if (highScore === null) {
+    highScore = 0;
+  }
+  //Set new high score if player has more money than current high score
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  }
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+  //If player is still alive, player wins!
   if (playerInfo.health > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
   }
@@ -160,6 +175,7 @@ var endGame = function() {
     window.alert("You've lost your robot in battle.");
   }
 
+  //Ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
   if (playAgainConfirm) {
